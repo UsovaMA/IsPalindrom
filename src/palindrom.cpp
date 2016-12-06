fad12f50e5404c11018a93283b92ed2273282695
Usova Marina A <oppabang@mail.ru>
2016-12-06 20:12:49 +0300

    Corrections

--- src/palindrom.cpp
+++ src/palindrom.cpp
@@ -1,46 +1,19 @@
 #include "palindrom.h"
 
 bool isPalindrom(CNode *head) {
-	int size = 0;
-	CNode* node = head;
+  int size = 0;
+  CNode* node = head;
 
-	while (node != 0) {
-		size++;
-		node = node->next;
-	}
+  while (node != 0) {
+    size++;
+    node = node->next;
+  }
 
-	/*int offset = size - 1;
+  int mid = size / 2;
 
-	for (int i = 0; i < size / 2; ++i) {
-	node = head;
-	for (int j = 0; j < offset; ++j)
-	node = node->next;
-
-	if (node->data != head->data) {
-	return false;
-	}
-
-	head = head->next;
-	node = node->next;
-	offset -= 2;
-	}
-
-	return true;
-	*/
-
-	node = head;
-	CNode *mas = new CNode[size];
-	int mid = size / 2;
-
-	for (int i = 0; i < size; i++) {
-		mas[i] = node->data;
-		node = node->next;
-	}
-
-	for (int i = 0; i < mid; i++) {
-		if (mas[i].data != mas[size - 1 - i].data)
-			return false;
-	}
-
-	return true;
+  for (int i = 0; i < mid; i++) {
+    if (head[i].data != head[size - 1 - i].data)
+      return false;
+  }
+  return true;
 }
